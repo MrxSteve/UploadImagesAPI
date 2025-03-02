1c0563fd84120ad2c9ec163b6c812ac01b19ffde
mrxSteve <steventobar38@gmail.com>
2025-03-01 18:51:24 -0600

    feat: configuracion inicial del proyecto

---
+++ src/main/java/com/stevedev/uploadimages/UploadimagesApplication.java
@@ -0,0 +1,13 @@
+package com.stevedev.uploadimages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadimagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadimagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stevedev/uploadimages/UploadimagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stevedev.uploadimages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadimagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
