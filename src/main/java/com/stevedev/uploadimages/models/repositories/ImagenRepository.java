c3e21690769b434d933c9648240030ff36cb875b
mrxSteve <steventobar38@gmail.com>
2025-03-02 23:40:40 -0600

    feat: buscar imagenes de un producto

--- src/main/java/com/stevedev/uploadimages/models/repositories/ImagenRepository.java
+++ src/main/java/com/stevedev/uploadimages/models/repositories/ImagenRepository.java
@@ -3,5 +3,8 @@ package com.stevedev.uploadimages.models.repositories;
 import com.stevedev.uploadimages.models.entities.ImagenEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ImagenRepository extends JpaRepository<ImagenEntity, Long> {
+    List<ImagenEntity> findByProducto_Id(Long productoId);
 }
