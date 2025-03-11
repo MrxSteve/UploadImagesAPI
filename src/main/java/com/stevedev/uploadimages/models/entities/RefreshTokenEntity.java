abe664f88ff0caf174d6baf3e66463b37c343184
mrxSteve <steventobar38@gmail.com>
2025-03-10 23:56:21 -0600

    feat: entidades login

---
+++ src/main/java/com/stevedev/uploadimages/models/entities/RefreshTokenEntity.java
@@ -0,0 +1,27 @@
+package com.stevedev.uploadimages.models.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "refresh_tokens")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+public class RefreshTokenEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(name = "expiry_date", nullable = false)
+    private Instant expiryDate;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+}
