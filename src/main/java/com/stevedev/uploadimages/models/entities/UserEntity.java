be7f1b14b8176ba1acfee3fdf2a10f1bc27c79b1
mrxSteve <steventobar38@gmail.com>
2025-03-14 23:57:16 -0600

    feat: entidades para el login

--- src/main/java/com/stevedev/uploadimages/models/entities/UserEntity.java
+++ src/main/java/com/stevedev/uploadimages/models/entities/UserEntity.java
@@ -23,7 +23,7 @@ public class UserEntity {
 
     private String password;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
