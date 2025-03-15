eb257f6fcb0527cbd2b4b058bf3f4b8cb9bb00f5
mrxSteve <steventobar38@gmail.com>
2025-03-14 23:57:51 -0600

    feat: responses

--- src/main/java/com/stevedev/uploadimages/models/dto/response/AuthResponse.java
+++ src/main/java/com/stevedev/uploadimages/models/dto/response/AuthResponse.java
@@ -1,11 +1,13 @@
 package com.stevedev.uploadimages.models.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor @AllArgsConstructor
+@Builder
 public class AuthResponse {
     private String token;
     private String refreshToken;
